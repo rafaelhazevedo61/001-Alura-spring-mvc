5ec310597a7005041895768b0af2d21f65a01363
rafael <rafaelhazevedo61@gmail.com>
2022-05-09 22:22:21 -0300

    aula 3 - completa - usando spring data jpa
    Signed-off-by: rafael <rafaelhazevedo61@gmail.com>

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,23 +2,19 @@ package br.com.alura.mvc.mudi.repository;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 
 @Repository
-public class PedidoRepository {
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+	
+//	public List<Pedido> findAll();
 
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public List<Pedido> recuperaTodosOsPedidos() {
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		return query.getResultList();
-	}
+//	public List<Pedido> recuperaTodosOsPedidos() {
+//		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+//		return query.getResultList();
+//	}
 
 }
