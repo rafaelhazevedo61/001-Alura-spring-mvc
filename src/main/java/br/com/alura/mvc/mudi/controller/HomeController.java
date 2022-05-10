8c4656bc9bf0a7b07544e49940ae2ae9736ce273
rafael <rafaelhazevedo61@gmail.com>
2022-05-09 22:05:17 -0300

    aula 3 - parcial - injeção de dependências
    Signed-off-by: rafael <rafaelhazevedo61@gmail.com>

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -2,40 +2,23 @@ package br.com.alura.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private PedidoRepository repository;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();
-		
-		// PREENCHIMENTO MANUAL
-//		  Pedido pedido = new Pedido();
-//        pedido.setNomeProduto("Monitor LG 29' IPS, Ultra Wide");
-//        pedido.setUrlImagem("https://images.kabum.com.br/produtos/fotos/157553/monitor-lg-led-29-ultrawide-ips-hdmi-displayport-hdr-com-vesa-e-alto-falante-amd-radeon-freesync-29wl500_1620660433_gg.jpg");
-//        pedido.setUrlProduto("https://www.kabum.com.br/produto/157553/monitor-lg-29-ips-ultra-wide-full-hd-hdmi-vesa-ajuste-de-angulo-hdr-10-99-srgb-freesync-29wl500");
-//        pedido.setDescricao("uma descrição qualquer para esse pedido");
-//        
-//        //USADOS PARA FINALIDADE DE TESTAR TODOS OS CAMPOS JÁ NESSA AULA
-//        pedido.setValorNegociado(new BigDecimal("1299.90"));
-//        pedido.setDataDaEntrega(LocalDate.of(2022, 5, 9));
-//
-//        List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
 		model.addAttribute("pedidos", pedidos);
 
 		return "home";
